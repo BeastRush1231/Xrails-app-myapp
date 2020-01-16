285290abca9bcc64299b5dbcbb43b363c911b603
BeastRush1231 <M0761005@g2.usc.edu.tw>
2020-01-16 16:32:00 +0800

    MSP_SSED_0116 Home component

--- app/javascript/components/Home/Home.js
+++ app/javascript/components/Home/Home.js
@@ -1,23 +1,22 @@
 import React from 'react';
 
 class Home extends React.Component {
-  // constructor(){
-  //   this.state = {
-  //     modules: [
-  //       { id: 1, title: '1. Setting up a new Ruby on Rails App with React.', description: '', active: false },
-  //       { id: 2, title: '2. Adding React to an Existing Rails App.', description: '', active: false },
-  //       { id: 3, title: '3. Building a Hello World App.', description: '', active: false },
-  //       { id: 4, title: '4. ', description: '', active: false }
-  //     ]
-  //   }
-  // }
+  constructor(){
+    super()
+
+    this.state = {
+      five_x_ruby_modules: [
+        { id: 1, title: '1. Setting up a new Ruby on Rails App with React.', description: 'lorem isupm', active: false },
+        { id: 2, title: '2. Adding React to an Existing Rails App.', description: 'lorem isupm', active: false },
+        { id: 3, title: '3. Building a Hello World App.', description: 'lorem isupm', active: false },
+        { id: 4, title: '4. ', description: 'lorem isupm', active: false }
+      ]
+    }
+  }
 
   render(){
     return (
-      <div className="bg-red-700 h-screen flex justify-around">
-        <div className="left">left</div>
-        <div className="right">right</div>
-      </div>
+      <div>This is our Home component.</div>
     )
   };
 }
